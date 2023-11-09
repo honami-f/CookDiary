519f85c355b03498eb9455bfe6af8a4f44b13bfb
honami-f <hnmr.o.s2@gmail.com>
2023-11-09 12:54:23 +0900

    コントローラー設定

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,20 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
   private
   def recipe_params
     params.require(:recipe).permit(:title, :ingredient, :instruction, :reference_url, :memo, :image).merge(user_id: current_user.id)
