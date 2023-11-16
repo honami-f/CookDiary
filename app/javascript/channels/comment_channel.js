2c95d372b85ce7cb0636ef41189c5e77db995d3f
honami-f <hnmr.o.s2@gmail.com>
2023-11-16 21:34:59 +0900

    Javascript設定

---
+++ app/javascript/channels/comment_channel.js
@@ -0,0 +1,29 @@
+import consumer from "channels/consumer"
+
+if(location.pathname.match(/\/recipes\/\d/)){
+
+  consumer.subscriptions.create({
+    channel: "CommentChannel",
+    recipe_id: location.pathname.match(/\d+/)[0]
+  }, {
+
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
+
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      const html = `
+        <div class="comment">
+          <p class="user-info">${data.user.name}： </p>
+          <p>${data.comment.text}</p>
+        </div>`
+      const comments = document.getElementById("comments")
+      comments.insertAdjacentHTML('beforeend', html)
+      const commentForm = document.getElementById("comment-form")
+      commentForm.reset();    }
+  })
+}
