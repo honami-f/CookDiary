cfae7c6155975ce6027d569e08d0883ceef81738
honami-f <hnmr.o.s2@gmail.com>
2023-12-03 21:55:37 +0900

    ルボコップ実行

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,13 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @recipe = Recipe.find(params[:recipe_id])
-    if @comment.save
-      CommentChannel.broadcast_to @recipe, { comment: @comment, user: @comment.user }    end
+    return unless @comment.save
+
+    CommentChannel.broadcast_to @recipe, { comment: @comment, user: @comment.user }
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, recipe_id: params[:recipe_id])
   end
