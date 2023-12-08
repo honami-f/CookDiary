86c8001d4f11727b2c470058a994f0a644dc3fa0
honami-f <hnmr.o.s2@gmail.com>
2023-12-08 21:32:44 +0900

    メソッド定義

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,7 +7,10 @@ class Recipe < ApplicationRecord
   validates :title, presence: true
   validates :ingredient, presence: { message: "can't be blank" }, if: ->(recipe) { recipe.instruction.present? }
   validates :instruction, presence: { message: "can't be blank" }, if: ->(recipe) { recipe.ingredient.present? }
-  validates :ingredient, :instruction, :reference_url, presence: { message: "can't be blank" }, if: lambda { |recipe|
-                                                                                                      recipe.ingredient.blank? && recipe.instruction.blank? && recipe.reference_url.blank?
-                                                                                                    }
+  validates :ingredient, :instruction, :reference_url, presence: { message: "can't be blank" },
+          if: lambda { |recipe| recipe.ingredient.blank? && recipe.instruction.blank? && recipe.reference_url.blank?}
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
